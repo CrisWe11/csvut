dee31b777b728f0077e056848df7d90e02639241
Criswe11 <fanana.bass@gmail.com>
2024-03-29 08:29:10 +0800

    bug fix & header support

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,9 @@ type MyResult = Result<(), std::io::Error>;
 
 const BUF_SIZE: usize = 1024;
 
-#[cfg(windows)]
-const NEWLINE: &str = "\r\n";
+const NEWLINE_LEN: usize = 1;
 
-#[cfg(not(windows))]
-const NEWLINE: &str = "\n";
-
-const BOM:[u8;3] = [0xEF, 0xBB, 0xBF];
+const BOM: [u8; 3] = [0xEF, 0xBB, 0xBF];
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -35,6 +31,11 @@ struct CsvUtil {
     #[clap(default_value = "./output")]
     output_path: String,
 
+    /// How many lines for header
+    #[arg(long)]
+    #[clap(default_value_t = 1)]
+    header: u8,
+
     /// Turn debugging info on
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
@@ -46,6 +47,7 @@ async fn main() -> MyResult {
     let input_path = Path::new(&csv_util.input_path);
     let output_path = Path::new(&csv_util.output_path);
     let file_lines = csv_util.file_lines;
+    let mut header = csv_util.header;
 
     if !input_path.exists() {
         println!("Cannot find input file!");
@@ -53,7 +55,7 @@ async fn main() -> MyResult {
     }
 
     match create_dir(output_path).await {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(_) => {
             println!("output-path already exists!");
             exit(2);
@@ -67,17 +69,23 @@ async fn main() -> MyResult {
     let mut bytes_size: usize = 0;
     let mut seek_from: u64 = 0;
     let mut file_suffix: usize = 0;
+    let mut header_line: String = String::from("");
 
     let mut set = JoinSet::new();
 
     while let Some(line) = lines.next_line().await? {
+        if header > 0 {
+            header -= 1;
+            header_line.push_str(&format!("{}\n", &line));
+            continue;
+        }
+
         line_counter += 1;
-        bytes_size += line.len() + NEWLINE.len();
-        if line_counter == file_lines {
-            set.spawn(process_parts(PathBuf::from(csv_util.input_path.clone()), PathBuf::from(csv_util.output_path.clone()), file_suffix.to_string(), SeekFrom::Start(seek_from), bytes_size, file_suffix > 0));
+        bytes_size += line.len() + NEWLINE_LEN;
+        if line_counter % file_lines == 0 {
+            set.spawn(process_parts(PathBuf::from(csv_util.input_path.clone()), PathBuf::from(csv_util.output_path.clone()), file_suffix.to_string(), SeekFrom::Start(seek_from), bytes_size, file_suffix > 0, header_line.clone()));
             seek_from += u64::try_from(bytes_size).unwrap();
             file_suffix += 1;
-            line_counter = 0;
             bytes_size = 0;
         }
     }
@@ -86,7 +94,7 @@ async fn main() -> MyResult {
     Ok(())
 }
 
-async fn process_parts(input_path: PathBuf, mut output_path: PathBuf, file_suffix: String, seek_from: SeekFrom, bytes_size: usize, need_bom: bool) -> MyResult {
+async fn process_parts(input_path: PathBuf, mut output_path: PathBuf, file_suffix: String, seek_from: SeekFrom, bytes_size: usize, need_bom: bool, header_line: String) -> MyResult {
     let input_file = File::open(&input_path).await?;
     let mut buf_reader = BufReader::new(input_file);
     output_path.push(input_path.file_stem().map(|n| format!("{}_{}.csv", OsString::from(n).to_str().unwrap(), file_suffix)).unwrap());
@@ -102,6 +110,7 @@ async fn process_parts(input_path: PathBuf, mut output_path: PathBuf, file_suffi
     if need_bom {
         buf_writer.write_all(&BOM).await?;
     }
+    buf_writer.write_all(header_line.as_bytes()).await?;
 
     while let Ok(size) = buf_reader.read(&mut buffer).await {
         if size == 0usize {
